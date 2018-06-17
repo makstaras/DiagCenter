8be50dcb60f5279beb4b4511ff5c4128959eb6b0
makstaras <makstaras@ukr.net>
2018-06-17 12:02:59 +0300

    Add files via upload

---
+++ sikulix.sikuli/sikulix.py
@@ -0,0 +1,10 @@
+click("1487619739541.png")
+type("doctor")
+click("1487619764852.png")
+click("1487620534869.png")
+type("admin")
+click("1487620642912.png")
+click("1487620246580.png")
+click("1487620264106.png")
+
+       
